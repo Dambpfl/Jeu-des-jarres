c22793f7448a0599ebe83f0d69026910a7fd5515
Damien <boenapfel.damien@yahoo.fr>
2026-02-24 14:27:05 +0100

    Add : condition de victoire/défaite - intéraction

--- niveau1.php
+++ niveau1.php
@@ -10,4 +10,25 @@ $jarres[] = "serpent";
 
 shuffle($jarres);
 
-print_r($jarres);
+echo "Selectionnez une jarre parmi les 5. Entrez un numéro de 1 à 5.";
+
+$choix = readline();
+
+if (!in_array($choix, ["1", "2", "3", "4", "5"])) {
+    echo "Le choix est invalide. Tapez un numéro entre 1 et 5.";
+    exit;
+}
+
+$index = $choix - 1;
+$resultat = $jarres[$index];
+
+if ($resultat === "clé") {
+    echo "Bien joué ! Vous avez gagné !\n";
+} else {
+    echo "Perdu, le serpent vous a mordu !\n";
+}
+
+echo "Les jarres contenaient :\n";
+foreach($jarres as $i => $contenu) {
+    echo "Jarre ".($i+1).": $contenu\n";
+}
